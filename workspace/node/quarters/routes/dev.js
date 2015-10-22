e7e3bb270e36fe5df2da8ca120a86758e7f6deb4
lyk2 <lyk2@mcmaster.ca>
2015-10-21 23:46:39 -0400

    basic monitor tools added

---
+++ workspace/node/quarters/routes/dev.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+var sys = require('util');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* GET users listing. */
+router.get('/update', function(req, res, next) {
+	var exec = require('child_process').exec,
+    child;
+
+	child = exec('quarters-web update',
+	  function (error, stdout, stderr) {
+	    res.send(stdout);
+	});
+});
+
+router.get('/status', function(req, res, next) {
+	var exec = require('child_process').exec,
+    child;
+
+	child = exec('quarters-web status',
+	  function (error, stdout, stderr) {
+	    res.send(stdout);
+	});
+});
+
+
+
+module.exports = router;
