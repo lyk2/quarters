1d7af212b5f31305bebff2a3e80431ea01393eca
chenw25 <chenw25@gmail.com>
2015-11-24 05:45:08 -0500

    added login feature

--- workspace/dev/routes/login.js
+++ workspace/dev/routes/login.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var pg = require("pg");
+var port = 5433;
+var host = 'system.space.quarters';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('login');
+router.get('/', function (req, res, next) {
+    res.render('login');
+});
+
+// target for form submit
+var conString = "pg://quarters:qadmin@system.quarters.space/quarters";
+var client = new pg.Client(conString);
+client.connect();
+
+router.post('/', function (req, response) {
+
+    var email = req.body.email;
+    var password = req.body.password;
+    var error = null;
+    var bcrypt = require('bcryptjs');
+    client.query("SELECT user_name,password from \"users\" WHERE user_name = $1", [email], function (err, result) {
+        if (result.rows.length == 0) {
+            error = 'Incorrect username/password';
+            console.log(error);
+            response.status(403);
+            response.render('login', {
+                error: error
+            });
+        } else {
+            if (!bcrypt.compareSync(password, result.rows[0].password)) {
+                error = 'Incorrect username/password.';
+                console.log(error);
+                response.status(403);
+                response.render('login', {
+                    error: error
+                });
+            } else {
+                console.log('user found');
+                response.redirect("../main");
+            }
+        }
+    });
 });
 
 module.exports = router;
--- workspace/dev/routes/signup.js
+++ workspace/dev/routes/signup.js
@@ -10,36 +10,6 @@ router.get('/', function (req, res, next) {
     //res.send('respond with a resource');
 });
 
-/*
- // ajax target for checking username
- router.post('/check/username', function(req, res) {
- var username = req.body.username;
- // check if username contains non-url-safe characters
- if (username !== encodeURIComponent(username)) {
- res.json(403, {
- invalidChars: true
- });
- return;
- }*/
-/*
- // check if username is already taken - query your db here
- var usernameTaken = false;
- for (var i = 0; i < dummyDb.length; i++) {
- if (dummyDb[i].username === username) {
- usernameTaken = true;
- break;
- }
- }
- if (usernameTaken) {
- res.json(403, {
- isTaken: true
- });
- return
- }
- // looks like everything is fine
- res.send(200);
- });
- */
 // target for form submit
 var conString = "pg://quarters:qadmin@system.quarters.space/quarters";
 var client = new pg.Client(conString);
@@ -47,40 +17,37 @@ client.connect();
 
 router.post('/', function (req, response) {
 
-
-    // var username = req.body.username;
     var email = req.body.email;
     var password = req.body.password;
-    var verification = req.body.verification;
     var error = null;
-    // regexp from https://github.com/angular/angular.js/blob/master/src/ng/directive/input.js#L4
-    var EMAIL_REGEXP = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}$/;
 
-    // check for valid inputs
-    if (!email || !password || !verification) {
-        error = 'All fields are required';
-    } else if (!email.match(EMAIL_REGEXP)) {
-        error = 'Email is invalid';
-    } else if (password !== verification) {
-        error = 'Passwords don\'t match';
+    client.query("SELECT user_name from \"users\" WHERE user_name = $1",[email], function(err, result) {
+        if (result.rows.length!=0){
+            error = 'Email is already taken, please try something else.';
+        }
+        errorOut(error);
+    });
+    function errorOut(error){
+        if(error){
+            response.status(403);
+            response.render('signup', {
+                error: error
+            });
+            return
+        }else{
+            // Load the bcrypt module
+            var bcrypt = require('bcryptjs');
+            // Generate a salt
+            var salt = bcrypt.genSaltSync(10);
+            // Hash the password with the salt
+            var hash = bcrypt.hashSync(password, salt);
+
+
+            client.query("INSERT INTO users(user_name, password) values($1, $2)", [email, hash]);
+            //client.end();
+            response.redirect("../main");
+        }
     }
-
-    if (error) {
-        response.status(403);
-        response.render('signup', {
-            error: error
-        });
-        return
-    }
-
-    // Load the bcrypt module
-    var bcrypt = require('bcrypt');
-    // Generate a salt
-    var salt = bcrypt.genSaltSync(10);
-    // Hash the password with the salt
-    var hash = bcrypt.hashSync(password, salt);
-
-    client.query("INSERT INTO users(user_name, password) values($1, $2)", [email, hash]);
 
 });
 
