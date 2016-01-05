0d89b511253fd2b1b4b1b010fe8106957432eed0
chenw25 <chenw25@gmail.com>
2016-01-05 14:26:14 -0500

    added discussion table

--- workspace/dev/createTables.sql
+++ workspace/dev/createTables.sql
@@ -8,3 +8,19 @@ create table houses(
 	address text,
 	postal_code text
 );
+
+CREATE TABLE discussion
+(
+  discussion_id integer NOT NULL,
+  house_id integer NOT NULL,
+  user_id integer NOT NULL,
+  post_date timestamp with time zone NOT NULL,
+  post_content text,
+  CONSTRAINT discussion_pkey PRIMARY KEY (discussion_id),
+  CONSTRAINT house_id_fkey FOREIGN KEY (house_id)
+      REFERENCES house (house_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE,
+  CONSTRAINT user_id_fkey FOREIGN KEY (user_id)
+      REFERENCES user_info (user_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
